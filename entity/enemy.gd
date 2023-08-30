2057d1d4c9f34ccd619e34e033d08627fa170924
exklamationmark <tonghuukhiem@gmail.com>
2023-08-29 23:45:40 -0700

    Enemy have random target names

--- entity/enemy.gd
+++ entity/enemy.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 @export var Speed:int = 100
+@export var TargetLabel:String = "CHANGE_ME"
 
 var target:Vector2 = self.position
 
+var name_list = [
+	"chicken",
+	"egg",
+	"foo",
+	"bar",
+	"hello world",
+	"random",
+]
+
 func _ready():
 	var mage = get_node("/root/root/Mage")
+
+	set_random_target_label()
 	set_target(mage)
 
 func _process(delta):
@@ -19,3 +31,8 @@ func move_towards(target:Vector2, distance:float):
 
 func set_target(target):
 	self.target = target.position
+
+func set_random_target_label():
+	var name = name_list[randi() % name_list.size()]
+	self.TargetLabel = name
+	$TargetLabel.text = name
