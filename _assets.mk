21a742a30d3aa0003b34a05de50588bc17807fb9
exklamationmark <tonghuukhiem@gmail.com>
2023-08-29 23:36:12 -0700

    Add assets for mage, enemy and spell

--- _assets.mk
+++ _assets.mk
@@ -1,11 +1,32 @@
 assets-all: assets
 assets-all: assets/logo.png
+assets-all: assets/mage.png
+assets-all: assets/enemy.png
+assets-all: assets/spell.png
 .PHONY: assets-all
 
 assets:
 	mkdir -p assets
 
 assets/logo.png: media/logo.aseprite
+	$(ASEPRITE) --batch \
+		media/$(basename $(notdir ${<})).aseprite \
+		--scale 1 \
+		--save-as assets/$(basename $(notdir ${@})).png
+
+assets/mage.png: media/mage.aseprite
+	$(ASEPRITE) --batch \
+		media/$(basename $(notdir ${<})).aseprite \
+		--scale 1 \
+		--save-as assets/$(basename $(notdir ${@})).png
+
+assets/enemy.png: media/enemy.aseprite
+	$(ASEPRITE) --batch \
+		media/$(basename $(notdir ${<})).aseprite \
+		--scale 1 \
+		--save-as assets/$(basename $(notdir ${@})).png
+
+assets/spell.png: media/spell.aseprite
 	$(ASEPRITE) --batch \
 		media/$(basename $(notdir ${<})).aseprite \
 		--scale 1 \
