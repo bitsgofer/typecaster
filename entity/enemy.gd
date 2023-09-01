a278e0dc2424b251bb601b3fab22e0c8821e571b
exklamationmark <tonghuukhiem@gmail.com>
2023-09-01 00:27:07 -0700

    Handle spell <-> enemy collision

--- entity/enemy.gd
+++ entity/enemy.gd
@@ -4,6 +4,21 @@ extends Node2D
 
 func _ready():
 	self.add_to_group("enemy")
+	$Enemy.area_entered.connect(Callable(self, "_on_spell_hit"))
 
 func _process(delta):
 	pass
+
+func _on_spell_hit(area:Area2D):
+	print_debug("ENEMY COLLISION: collied with %s" % area.name)
+	match (area.name):
+		"Spell":
+			collide_with_spell()
+		_:
+			print_debug("ENEMY COLLISION: collied with unhandled area")
+			return
+
+func collide_with_spell():
+	print_debug("ENEMY COLLISION: enemy/%s && groupped spell => destroyed" % self.get_instance_id())
+	get_tree().call_group("spell/%s" % self.target_name, "queue_free")
+	self.queue_free()
