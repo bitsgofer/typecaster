f91731c9730b139193517fd7f963f09b41ae3661
exklamationmark <tonghuukhiem@gmail.com>
2023-09-01 00:11:13 -0700

    Shoot spell towards targets
    - When enemy spawn, add them to a group.
    - When executing incantation, search within enemy group and find the one
      with matching 'target_name'.
    - If a match is found, create a spell and shoot towards the enemy.

---
+++ entity/enemy.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@export var target_name = "CHANGE_ME"
+
+func _ready():
+	self.add_to_group("enemy")
+
+func _process(delta):
+	pass
--- scene/game.gd
+++ scene/game.gd
@@ -39,11 +39,25 @@ func update_incantation(key:Key):
 
 func execute_incantation(incantation:String):
 	print_debug("EXECUTE: %s" % incantation)
-	cast_spell()
+	var target = find_matching_enemy(incantation)
+	if target == null:
+		print_debug("NOT FOUND: target=null")
+		return
+	self.cast_spell(target)
+
+func find_matching_enemy(targetName:String) -> Node:
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	for enemy in enemies:
+		print_debug("SEARCH: enemy/%s; target_name= %s" % [enemy.get_instance_id(), enemy.target_name])
+		if enemy.target_name.to_upper() == targetName.to_upper():
+			print_debug("FOUND: enemy/%s; target_name= %s" % [enemy.get_instance_id(), enemy.target_name])
+			return enemy
+	print_debug("NOT FOUND: enemy=null")
+	return null
 
 const SpellScene = preload("res://entity/spell.tscn")
-func cast_spell():
+func cast_spell(target:Node):
 	var spell = SpellScene.instantiate()
-	spell.configure(Vector2(300, 200), null, 20)
+	spell.configure($Mage.position, target, 200)
 	print_debug("SPAWN: spell/%s @ (%d, %d)" % [spell.get_instance_id(), spell.position.x, spell.position.y])
 	self.add_child(spell)
