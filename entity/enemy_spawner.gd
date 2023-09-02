56eff2e40b1dacb717ce6d3efb00b313b65e4cc4
exklamationmark <tonghuukhiem@gmail.com>
2023-09-01 23:52:44 -0700

    Make entity/enemy_spawner's spawn position configurable
    - Enemies are spawned in a donut shape that can be configured in the
      editor.

--- entity/enemy_spawner.gd
+++ entity/enemy_spawner.gd
@@ -7,9 +7,17 @@ extends Node2D
 @export var entity_type:String = "enemy_spawner"
 @export var spawn_interval:float = 3
 @export var target:Node = null
+@export var enabled:bool = false
+@export_range(48, 144, 1, "or_greater", "or_less") var spawn_donut_inner_radius:int = 48
+@export_range(272, 528, 1, "or_greater", "or_less") var spawn_donut_outer_radius:int = 272
+@export_range(0, 360, 1, "degrees") var spawn_donut_min_degree:int = 0
+@export_range(0, 360, 1, "degrees") var spawn_donut_max_degree:int = 360
+
+func _enter_tree():
+	$Timer.autostart = self.enabled
+	$Timer.wait_time = self.spawn_interval
 
 func _ready():
-	$Timer.wait_time = self.spawn_interval
 	$Timer.timeout.connect(Callable(self, "_on_spawn_timer_ticked"))
 
 func configure(p_global_position:Vector2, p_target:Node, p_spawn_interval:float):
@@ -23,16 +31,18 @@ func _on_spawn_timer_ticked():
 		print_debug("ENEMY_SPAWNER.V(4): enemy_spawner/%s (target= %s): self.target is null => skip spawning enemy" % [self.name, self.target.name])
 		return
 	var _enemy = enemy_scene.instantiate()
-	var _position = get_random_position_within_donut_shape(self.global_position, 16, 64)
+	var _position = get_random_position_within_donut(self.global_position)
 	var _identifier = get_random_name()
 	_enemy.configure(_position, self.target, 20, _identifier)
 	self.add_child(_enemy)
 
-func get_random_position_within_donut_shape(center:Vector2, inner_radius:float, outter_radius:float) -> Vector2:
-	var distance = inner_radius + (randf() * (outter_radius - inner_radius))
-	var angle = randf() * 2 * PI
-	var offset = Vector2(cos(angle), sin(angle)) * distance
-	return center + offset
+func get_random_position_within_donut(p_center:Vector2) -> Vector2:
+	var _distance = self.spawn_donut_inner_radius + (randf() * (self.spawn_donut_outer_radius - self.spawn_donut_inner_radius))
+	var _angle_in_degree = self.spawn_donut_min_degree + randf() * (self.spawn_donut_max_degree - self.spawn_donut_min_degree)
+	var _angle = _angle_in_degree/180.0*PI
+	print_debug("ENEMY_SPAWNER.V(4): random position with angle=%f (radian)/%f (degree), distance= %f" % [_angle, _angle_in_degree, _distance])
+	var _offset = Vector2(cos(_angle), sin(_angle)) * _distance
+	return p_center + _offset
 
 var target_name_list = [
 	"a",
