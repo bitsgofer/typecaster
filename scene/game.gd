c02949b1e157c27775537a7a578a609853b88f42
exklamationmark <tonghuukhiem@gmail.com>
2023-09-03 15:00:13 -0700

    Make background music loop

--- scene/game.gd
+++ scene/game.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _ready():
 	print_debug("SCENE.V(0): Ready: scene/game")
+	$BackgroundMusic.connect("finished", Callable(self, "_on_background_music_finished"))
+
+func _on_background_music_finished():
+	$BackgroundMusic.play()
